9b0b7af844021765ed58c06cfd723b00a76aaca8
clarissa1110 <clarissapiovesan@gmail.com>
2024-07-07 23:34:23 +0200

    feat: implemented updateUserInfo

--- src/app/components/update-user-info/update-user-info.component.ts
+++ src/app/components/update-user-info/update-user-info.component.ts
@@ -1,10 +1,56 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { NgForm } from '@angular/forms';
+import { UserService } from 'src/app/services/user.service';
+import { AuthService } from 'src/app/auth/auth.service';
+import { User } from 'src/app/models/user.interface'; 
+
 
 @Component({
   selector: 'app-update-user-info',
   templateUrl: './update-user-info.component.html',
   styleUrls: ['./update-user-info.component.scss']
 })
-export class UpdateUserInfoComponent {
+export class UpdateUserInfoComponent implements OnInit {
+
+  user: User = {
+    userId: 0,
+    username: '',
+    firstName: '',
+    lastName: '',
+    email: '',
+    password: '',
+    avatarUrl: '',
+    bookshelves: []
+  }
+
+  constructor(private userSrv: UserService, private authSrv: AuthService) {}
+
+  ngOnInit(): void {
+    this.userSrv.getUserProfile().subscribe(
+      (data: User) => {
+        this.user = data;
+      },
+      (error) => {
+        console.log('Error fetching user profile', error);
+        
+      }
+    ) 
+  }
+
+  onSubmit(form: NgForm): void {
+    if (form.valid) {
+      this.userSrv.updateUserProfile(this.user.userId, this.user).subscribe(
+        (updatedUser: User) => {
+          console.log('User updated successfully', updatedUser);
+          alert('User info updated successfully!');
+        },
+        (error) => {
+          console.log('Error updating user', error);
+          
+        }
+      )
+    }
+  }
+
 
 }
--- src/app/models/user.interface.ts
+++ src/app/models/user.interface.ts
@@ -2,9 +2,11 @@ import { Bookshelf } from "./bookshelf.interface";
 
 export interface User {
     userId: number,
+    username: string,
     firstName: string,
     lastName: string,
     email: string,
+    password: string,
     avatarUrl: string,
     bookshelves: Bookshelf[]
 }
